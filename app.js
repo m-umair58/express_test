c01b7e17cc404147b11ccc23616333710e0e5bc8
Muhammad Umair <mianumair1958@gmail.com>
2024-10-14 20:47:34 +0500

    added get and post requests

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let fs = require('fs');
 let app = express();
 let movies = JSON.parse(fs.readFileSync('./data/movies.json'))
 
+app.use(express.json())
+
 app.get('/api/v1/movies',(req,res)=>{
     res.status(200).json({
         status:"success",
@@ -14,6 +16,22 @@ app.get('/api/v1/movies',(req,res)=>{
     });
 });
 
+app.post('/api/v1/movies',(req,res)=>{
+    const newId = movies[movies.length-1].id+1;
+
+    const newMovie = Object.assign({id:newId},req.body);
+
+    movies.push(newMovie);
+
+    fs.writeFile('./data/movies.json',JSON.stringify(movies),(err)=>{
+        res.status(201).json({
+            status:"success",
+            data:{
+                movie:newMovie
+            }
+        })
+    })
+})
 
 port = 8000
 app.listen(port,()=>{
