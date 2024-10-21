8456ee692c652de1f4d030709cedbf6dcef77867
Muhammad Umair <mianumair1958@gmail.com>
2024-10-21 21:37:49 +0500

    added sentry

---
+++ instrument.js
@@ -0,0 +1,14 @@
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+  dsn: "https://7d852c1e36c2f5f99a531c39b9f65c8e@o4508161644429312.ingest.de.sentry.io/4508161647640656",
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
